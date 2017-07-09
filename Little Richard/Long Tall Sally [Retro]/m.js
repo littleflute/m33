9936dfd601672ed9a69920b437f9fca6056ace95
littleflute <13699175164@sina.cn>
2017-07-09 13:11:28 -0500

    Add files via upload

---
+++ Little Richard/Long Tall Sally [Retro]/m.js
@@ -0,0 +1,12 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Long Tall Sally.mp3";		
+f[2]	= "02 Tutti Frutti.mp3";		
+f[3]	= "03 Good Golly Miss Molly.mp3";		
+f[4]	= "05 Jenny, Jenny.mp3";		
+f[5]	= "06 Keep a Knockin'.mp3";		
+f[6]	= "07 Rip It Up.mp3";		
+f[7]	= "08 Slippin' and Slidin'.mp3";		
+f[8]	= "09 Ooh My Soul.mp3";		
+f[9]	= "10 Baby Face.mp3";		
+f[10]	= "Lucille.mp3";		
