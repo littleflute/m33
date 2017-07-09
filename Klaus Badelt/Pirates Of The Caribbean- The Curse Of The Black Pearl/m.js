95dbf8d6cfb0e712d6c0191a24750a0f48005231
littleflute <13699175164@sina.cn>
2017-07-09 13:10:22 -0500

    Add files via upload

---
+++ Klaus Badelt/Pirates Of The Caribbean- The Curse Of The Black Pearl/m.js
@@ -0,0 +1,17 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Fog Bound.mp3";		
+f[2]	= "02 The Medallion Calls.mp3";		
+f[3]	= "03 The Black Pearl.mp3";		
+f[4]	= "04 Will And Elizabeth.mp3";		
+f[5]	= "05 Swords Crossed.mp3";		
+f[6]	= "06 Walk The Plank.mp3";		
+f[7]	= "07 Barbossa Is Hungry.mp3";		
+f[8]	= "08 Blood Ritual.mp3";		
+f[9]	= "09 Moonlight Serenade.mp3";		
+f[10]	= "10 To The Pirates' Cave.mp3";		
+f[11]	= "11 Skull And Crossbones.mp3";		
+f[12]	= "12 Bootstrap's Bootstraps.mp3";		
+f[13]	= "13 Underwater March.mp3";		
+f[14]	= "14 One Last Shot.mp3";		
+f[15]	= "15 He's A Pirate.mp3";		
